745159b85ebda65431a396e1b2bf8bb8a993f214
porto <idbi.developer@gmail.com>
2025-08-08 20:48:33 +0200

    Changes by kovacs.balazs.roland

--- src/main/java/me/idbi/spaceadventure/scene/SceneUpdater.java
+++ src/main/java/me/idbi/spaceadventure/scene/SceneUpdater.java
@@ -15,12 +15,6 @@ public class SceneUpdater implements Runnable {
             if(Main.getSceneManager().getCurrentScene() == null)
                 continue;
             long unixTime = System.currentTimeMillis();
-//            120 30ion in thread "Thread-2" java.lang.NullPointerException: Cannot invoke "me.idbi.spaceadventure.table.Table.getRend
-//            Terminal Size error!!!turn value of "me.idbi.spaceadventure.scene.SceneManager.getTable()" is null.getSceneManager()" is
-//            120 30
-//            Terminal Size error!!!ceadventure.scene.MainMenuScene.draw(MainMenuScene.java:49))
-//            120 30  at me.idbi.spaceadventure.scene.SceneManager.draw(SceneManager.java:37)1)
-//            Terminal Size error!!!ceadventure.scene.SceneUpdater.run(SceneUpdater.java:27)
             if(lastUpdateClear < unixTime) {
                 lastUpdateClear = unixTime + 2000L;
                 //Main.getSceneManager().draw(true);
